e9d60410a9a5f3ae8690108a4633c3acbbfc2552
Harish Vijay V <harishvijay0204@gmail.com>
2026-04-28 17:43:38 +0530

    updated

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import sqlite3
 
 DB_FILE = 'hospital_management.db'
@@ -19,3 +20,73 @@ def create_tables():
     run_query("CREATE TABLE IF NOT EXISTS Prescription (id INTEGER PRIMARY KEY, d_id INTEGER, p_id INTEGER, meds TEXT)")
     
     run_query("INSERT OR IGNORE INTO Admin (username, password) VALUES ('admin', 'admin123')")
+=======
+import sqlite3
+
+DB_FILE = 'hospital_management.db'
+
+def run_query(query, params=(), fetch=False):
+    with sqlite3.connect(DB_FILE) as conn:
+        cursor = conn.cursor()
+        cursor.execute(query, params)
+        if fetch:
+            return cursor.fetchall()
+        conn.commit()
+
+def create_tables():
+    run_query("CREATE TABLE IF NOT EXISTS Admin (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT)")
+    run_query("CREATE TABLE IF NOT EXISTS Doctor (id INTEGER PRIMARY KEY, name TEXT, username TEXT UNIQUE, password TEXT, status TEXT DEFAULT 'available')")
+    run_query("CREATE TABLE IF NOT EXISTS Patient (id INTEGER PRIMARY KEY, name TEXT, username TEXT UNIQUE, password TEXT)")
+    
+    run_query("CREATE TABLE IF NOT EXISTS Appointment (id INTEGER PRIMARY KEY, p_id INTEGER, d_id INTEGER, date TEXT, time TEXT, FOREIGN KEY(p_id) REFERENCES Patient(id), FOREIGN KEY(d_id) REFERENCES Doctor(id))")
+    
+    run_query("INSERT OR IGNORE INTO Admin (username, password) VALUES ('admin', 'admin123')")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # run_query("CREATE TABLE IF NOT EXISTS Prescription (id INTEGER PRIMARY KEY, d_id INTEGER, p_id INTEGER, meds TEXT)")
+>>>>>>> 663bbdb (updated)
